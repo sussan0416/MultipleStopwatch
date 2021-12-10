27bff05320db00876dcbb4f23d219f6cef309d19
sussan0416 <5867126+sussan0416@users.noreply.github.com>
2021-12-10 10:33:35 +0900

    define button pins

--- StopWatch.ino
+++ StopWatch.ino
@@ -1,6 +1,18 @@
 #include "CustomTypes.h"
 #include "Ticker.h"
 
+#define BUTTON_1_PIN 36
+#define BUTTON_2_PIN 39
+#define BUTTON_3_PIN 34
+#define BUTTON_4_PIN 35
+#define BUTTON_5_PIN 32
+
+#define BUTTON_1_LED_OUT_PIN 33
+#define BUTTON_2_LED_OUT_PIN 25
+#define BUTTON_3_LED_OUT_PIN 26
+#define BUTTON_4_LED_OUT_PIN 27
+#define BUTTON_5_LED_OUT_PIN 14
+
 // Measurement
 unsigned long measured_seconds[] = {0, 0, 0, 0, 0};
 Measure measure_states[] = {
@@ -17,16 +29,23 @@ Button button_states[] = {
   Button::Up, Button::Up,
   Button::Up
 };
-int button_pins[] = {32, 33, 25, 26};
+int button_pins[] = {
+  BUTTON_1_PIN, BUTTON_2_PIN,
+  BUTTON_3_PIN, BUTTON_4_PIN, BUTTON_5_PIN
+};
+int button_led_pins[] = {
+  BUTTON_1_LED_OUT_PIN,
+  BUTTON_2_LED_OUT_PIN,
+  BUTTON_3_LED_OUT_PIN,
+  BUTTON_4_LED_OUT_PIN,
+  BUTTON_5_LED_OUT_PIN
+};
 const short button_short_count = 3;
 const short button_long_count = 200;
-int button_led_pins[] = {14, 13, 23, 22
-};
 
 // Tickers
-Ticker measurementTicker;
-Ticker printMeasureTicker;
-Ticker suspendTicker;
+Ticker measurement_ticker;
+Ticker print_measure_ticker;
 
 // fired every 1/10 second.
 void measure() {
@@ -64,8 +83,8 @@ void setup() {
   }
 
   // measure every 0.1 seconds.
-  measurementTicker.attach(0.1, measure);
-  printMeasureTicker.attach(1.0, printMeasure);
+  measurement_ticker.attach(0.1, measure);
+  print_measure_ticker.attach(1.0, printMeasure);
 }
 
 void loop() {
